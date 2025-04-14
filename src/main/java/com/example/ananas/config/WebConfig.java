431598bed6723114371ad663b518ec91c6cc5e83
KhaiDo <khaiquocoai@gmail.com>
2025-04-14 21:51:55 +0700

    init project

---
+++ src/main/java/com/example/ananas/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.example.ananas.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    //file:///C:/Users/dater/Documents/HK7/Ananas/ananas/upload/user/
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/avatar/**").addResourceLocations("file:///D:/prj java/ananas/upload/user/")
+                .setCachePeriod(3600);
+        registry.addResourceHandler("/images/product/**").addResourceLocations("file:///D:/prj java/ananas/upload/product/")
+                .setCachePeriod(3600);
+    }
+}
+
