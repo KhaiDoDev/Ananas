e1ef94c74770e0cbba2b7bc2d3965e6aca7d73f6
KhaiDo <khaiquocoai@gmail.com>
2025-05-27 20:45:58 +0700

    add new feature

--- src/main/java/com/example/ananas/config/WebConfig.java
+++ src/main/java/com/example/ananas/config/WebConfig.java
@@ -10,9 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     //file:///C:/Users/dater/Documents/HK7/Ananas/ananas/upload/user/
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/images/avatar/**").addResourceLocations("file:///D:/prj java/ananas/upload/user/")
+        registry.addResourceHandler("/images/avatar/**").addResourceLocations("file:///D:/Project_java/backend/upload/user/")
                 .setCachePeriod(3600);
-        registry.addResourceHandler("/images/product/**").addResourceLocations("file:///D:/prj java/ananas/upload/product/")
+        registry.addResourceHandler("/images/product/**").addResourceLocations("file:///D:/Project_java/backend/upload/product/")
                 .setCachePeriod(3600);
     }
 }
